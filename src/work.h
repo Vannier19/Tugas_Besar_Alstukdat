a87ba9cc72d99d06fd80e52dca7f3cb6639afc71
valereoo <valereo3201@gmail.com>
2024-11-22 15:20:34 +0700

    Feat: Membuat command work

---
+++ src/work.h
@@ -0,0 +1,15 @@
+#ifndef KERJAAN
+#define KERJAAN
+
+#include <stdio.h>
+#include <time.h>
+#include "../ADT/mesinkata.h"
+#include "../save/save.h"
+
+void work();
+/*  I.S. Pengguna memiliki daftar pekerjaan yang tersedia untuk dipilih.
+    F.S. Pekerjaan selesai diproses:
+    - Jika input valid, pendapatan ditambahkan ke akun pengguna.
+    - Jika input tidak valid, pesan kesalahan ditampilkan.
+*/
+#endif
